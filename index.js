a73d9d0ae0f6ae299f77c9f06c67ebd96e5568e7
Misha Husak <diokrittdiokritt@gmail.com>
2020-07-08 12:09:53 +0300

    Beta-animation added

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ window.onload = function(){
                   this._super();  
                   //Getting Window Size
                   var size = cc.director.getWinSize(),
-                  //Label Data (Win / Not WIn)
+                  //Label Data , it will contain (Win / Not WIn)
                       resultOfSpin = "",
                   //Our Spinning Status, False by default
                       spinning = false,
+                  //Get random int function
+                      getRandomInt = function(int){
+                        return Math.random() * int;
+                      },
                   //Creating textures(sprites) variables
                       label = cc.LabelTTF.create("Result: " + resultOfSpin, "Arial", 40),
                       spinButton = new ccui.Button.create("resources/button_enabled.png"),
                       mesh = new cc.Sprite("resources/mesh.png"),
                       line = new cc.DrawNode();
                   //Creating function for positioning, scaling and addChild for our Textures(sprites)
-                  var self = this; //We don't want to lose context
+                  var self = this; //We don't want to lose this
                   var textureCustomizer = function (texture, xAxis, yAxis, scaleValue, z_Index) {
                     texture.setPosition(size.width / xAxis, size.height / yAxis);
                     texture.setScale(scaleValue);
@@ -32,24 +36,23 @@ window.onload = function(){
                   //Winning Indicator Red Line
                   line.drawSegment(cc.p(size.width / 8.4, size.height / 1.8), cc.p(size.width / 1.07, size.height / 1.8), 2 , cc.color("#D30000"));
                   this.addChild(line, 2);
-                  //Extracting icons from Plist
+                  //Extracting icons from Icons.Plist
                   cc.spriteFrameCache.addSpriteFrames("resources/icons.plist");
                   //Creating array of Slot Cells
                   var cells = [];
                   //Filling it with sprites
                   for (var index = 0; index < 9; index++) {
-                    cells.push(new cc.Sprite(cc.spriteFrameCache.getSpriteFrame("icons/" + Math.floor(Math.random() * 8) + ".png")));
+                    cells.push(new cc.Sprite(cc.spriteFrameCache.getSpriteFrame("icons/" + Math.floor(getRandomInt(8)) + ".png")));
                     this.addChild(cells[index], 1);
                     cells[index].setScale(1);
                   }
-                  
-                  //Cells positioning function for Rows
+                  //Cells positioning by Rows function 
                   var cellsPositioning = function(initialIndex, maxIndex, yAxisIndex) {
                     var Y_pos = [1.32, 1.8, 2.8],
                         X_pos = [4, 1.9, 1.25],
                         pos_index = 0;
-                    for (index = initialIndex; index < maxIndex; index++) {
-                      cells[index].setPosition(size.width / X_pos[pos_index], size.height / Y_pos[yAxisIndex]);
+                    for (var idx = initialIndex; idx < maxIndex; idx++) {
+                      cells[idx].setPosition(size.width / X_pos[pos_index], size.height / Y_pos[yAxisIndex]);
                       pos_index++;
                     }
                   }
@@ -71,15 +74,18 @@ window.onload = function(){
                     //There is the fun begin :D
                     var spinner = setInterval(function(){
                       for (index = 0; index < cells.length; index++) {
-                        //Refreshing cells with random Icons once in 100ms
-                        cells[index].id = Math.floor(Math.random() * 8);
-                        cells[index].initWithSpriteFrameName("icons/" + cells[index].id + ".png");
-                        cc.log(cells[index].id);
+                        cells[index].id = Math.floor(getRandomInt(8));
+                        cells[index].initWithSpriteFrameName("icons/" + cells[index].id + ".png"); 
+                        //It's just the illusion of spin effect, the animation is the most weak part of the project, it can be better 
+                        cells[index].runAction(cc.moveBy(0.1, 0 , -10));
+                        cellsPositioning(0,3,0);
+                        cellsPositioning(3,6,1);
+                        cellsPositioning(6,9,2);
                       }
-                    }, 100)
+                    }, 200)
                     //Stop Spin in 10 Sec
                     setTimeout(function() { 
-                      //Stop SetInterval
+                      //Stop SetInterval function
                       clearInterval(spinner); 
                       //Checking for Winning combo
                       checkWin();
@@ -96,24 +102,18 @@ window.onload = function(){
                       && cells[4].id === 5 
                       && cells[5].id === 5) 
                     {
-
                       resultOfSpin = "Not Win";
-
                     } //If 3 same cells - WIN
                     else if (cells[3].id === cells[4].id 
                       && cells[3].id === cells[5].id 
                       && cells[4].id === cells[5].id) 
                     {
-
                       resultOfSpin = "Win";
-
                     }
                       //Else 3 different cells - NOT WIN
                     else 
                     {
-
                       resultOfSpin = "Not Win";
-
                     }
                     label.setString("Result: " + resultOfSpin);
                   }
@@ -121,7 +121,7 @@ window.onload = function(){
                   var touchEvent = function (sender,  type) {
                     switch (type) {
                       case ccui.Widget.TOUCH_ENDED:
-                        //Increase scale on click for better UX
+                      //Increase scale on click for better UX
                       spinButton.setScale(1.5);
                       //StartSpin
                       makeSpin();
